041fa2d53d2b4239e5fd5a791b35c2ce4ac83cf2
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2023-03-15 16:04:24 +0100

    iqueryable extension

--- src/RuleFilterParser/Extensions/CollectionsExtensions.cs
+++ src/RuleFilterParser/Extensions/CollectionsExtensions.cs
@@ -7,4 +7,10 @@ public static class CollectionsExtensions
         var exp = FilterToLambdaParser.Parse<T>(filter);
         return source.Where(exp.Compile());
     }
+    
+    public static IQueryable<T> ApplyRuleFilter<T>(this IQueryable<T> source, Filter filter)
+    {
+        var exp = FilterToLambdaParser.Parse<T>(filter);
+        return source.Where(exp);
+    }
 }
