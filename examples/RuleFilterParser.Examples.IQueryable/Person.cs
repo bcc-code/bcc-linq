36287a8d47f194c021d555f8d135c1dd1b4425e4
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2023-03-16 11:52:31 +0100

    added datetime to example

--- examples/RuleFilterParser.Examples.IQueryable/Person.cs
+++ examples/RuleFilterParser.Examples.IQueryable/Person.cs
@@ -5,10 +5,12 @@ public class Person
     public int Id { get; set; }
     public string Name { get; set; }
     public int Age { get; set; }
+    public DateTime SomeDate { get; set; }
 
-    public Person(string name, int age)
+    public Person(string name, int age, DateTime someDate)
     {
         Name = name;
         Age = age;
+        SomeDate = someDate;
     }
 }
