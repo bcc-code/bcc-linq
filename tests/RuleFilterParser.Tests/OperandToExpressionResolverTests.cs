55c31890a0c54a6832d19639b51a86bb5cdf76d1
MarioCzyz <mariusz.czyz@bcc.no>
2023-05-15 11:48:57 +0200

    Should expect a stings from array, not the array of strings

--- tests/RuleFilterParser.Tests/OperandToExpressionResolverTests.cs
+++ tests/RuleFilterParser.Tests/OperandToExpressionResolverTests.cs
@@ -266,7 +266,7 @@ public class OperandToExpressionResolverTests
     public void should_return_array_contains_expression()
     {
         var parameter = Expression.Parameter(typeof(TestClass), "x");
-        var prop = Expression.Property(parameter, "stringArrayProp");
+        var prop = Expression.Property(parameter, "StrProp");
 
         var exp = OperandToExpressionResolver.GetExpressionForRule(
             prop, "_in", new[] { "a", "b" });
@@ -292,7 +292,7 @@ public class OperandToExpressionResolverTests
     public void should_return_array_not_contains_expression()
     {
         var parameter = Expression.Parameter(typeof(TestClass), "x");
-        var prop = Expression.Property(parameter, "stringArrayProp");
+        var prop = Expression.Property(parameter, "StrProp");
 
         var exp = OperandToExpressionResolver.GetExpressionForRule(
             prop, "_nin", new[] { "a", "b" });
