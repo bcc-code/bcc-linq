ad2b0fe45c5da96a469f32cbdb1c78a87e7b1d08
Leo Schick <leonhard.schick@gmail.com>
2023-06-18 14:54:02 +0200

    add missing local files #28 #40

---
+++ src/BccCode.Linq/Extensions/Enumerable.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+
+namespace System.Linq.Internal;
+
+/// <summary>
+/// Internal. Holds reflection helper for extension methods of class <see cref="System.Linq.Enumerable"/>.
+/// </summary>
+internal static class Enumerable
+{
+    internal static readonly MethodInfo FirstMethodInfo
+        = typeof(System.Linq.Enumerable)
+            .GetTypeInfo().GetDeclaredMethods(nameof(System.Linq.Enumerable.First))
+            .Single(
+                mi => mi.GetGenericArguments().Length == 1
+                      && mi.GetParameters().Length == 1);
+
+    internal static readonly MethodInfo FirstOrDefaultMethodInfo
+        = typeof(System.Linq.Enumerable)
+            .GetTypeInfo().GetDeclaredMethods(nameof(System.Linq.Enumerable.FirstOrDefault))
+            .Single(
+                mi => mi.GetGenericArguments().Length == 1
+                      && mi.GetParameters().Length == 1);
+    
+    internal static readonly MethodInfo SelectMethodInfo
+        = typeof(System.Linq.Enumerable)
+            .GetTypeInfo().GetDeclaredMethods(nameof(System.Linq.Enumerable.Select))
+            .Single(
+                mi => mi.GetGenericArguments().Length == 2
+                      && mi.GetParameters().Length == 2
+                      && mi.GetParameters()[1].ParameterType.GetGenericArguments().Length == 2);
+}
---
+++ src/BccCode.Linq/QueryProvider/IIncludableQueryable.cs
@@ -0,0 +1,15 @@
+﻿namespace BccCode.Linq;
+
+/// <summary>
+/// Supports queryable Include/ThenInclude chaining operators.
+/// </summary>
+/// <typeparam name="TEntity">The entity type.</typeparam>
+/// <typeparam name="TProperty">The property type.</typeparam>
+/// <remarks>
+/// This is a clone of
+/// <a href="https://learn.microsoft.com/en-us/dotnet/api/microsoft.entityframeworkcore.query.iincludablequeryable-2?view=efcore-7.0">
+/// Microsoft.EntityFrameworkCore.Query.IIncludableQueryable</a>
+/// </remarks>
+public interface IIncludableQueryable<out TEntity, out TProperty> : IQueryable<TEntity>
+{
+}
