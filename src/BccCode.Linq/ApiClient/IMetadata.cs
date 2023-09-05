944af7088b77e2db55273390730cbfde89caf1ca
Leo Schick <leonhard.schick@gmail.com>
2023-09-05 20:52:30 +0200

    drop extension property from IMetadata

--- src/BccCode.Linq/ApiClient/IMetadata.cs
+++ src/BccCode.Linq/ApiClient/IMetadata.cs
@@ -5,34 +5,27 @@ public interface IMetadata : IReadOnlyDictionary<string, object>
     /// <summary>
     /// Returns the page limit of the request.
     /// </summary>
-    public int Limit =>
-        this.TryGetValue("limit", out var limit) ? (int)limit : default;
+    public int Limit { get; }
 
     /// <summary>
     /// Returns how many rows have been skipped from the start.
     /// </summary>
-    public int Skipped =>
-        this.TryGetValue("skipped", out var skipped) ? (int)skipped : default;
+    public int Skipped { get; }
 
     /// <summary>
     /// Returns the number of rows, passed by the metadata property <c>"total"</c>.
     /// </summary>
-    [Obsolete($"Please use either property {nameof(FilterCount)} instead.")]
-    public int Total =>
-        this.TryGetValue("total", out var total) ? (int)total : default;
+    [Obsolete($"Please use property {nameof(FilterCount)} instead.")]
+    public long Total { get; }
 
     /// <summary>
     /// Returns the item count of the collection you're querying, taking the current filter/search parameters into account
     /// if given, otherwise <c>null</c>.
     /// </summary>
-    public long? FilterCount =>
-        this.TryGetValue("filter_count", out var filterCount) ? (long?)filterCount
-        : this.TryGetValue("total", out var total) ? (int)total
-        : null;
+    public long? FilterCount { get; }
 
     /// <summary>
     /// Returns the total item count of the collection you're querying if given, otherwise <c>null</c>.
     /// </summary>
-    public long? TotalCount =>
-        this.TryGetValue("total_count", out var totalCount) ? (long?)totalCount : null;
+    public long? TotalCount { get; }
 }
